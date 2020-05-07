81110439257a1a03ed5c3ad081cff0ca243c7d1a
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-05-07 13:07:15 -0500

    day 1 mvp

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
+import textwrap
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,14 +40,27 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Andrew", room['outside'])
+
+done = False
 
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+while not done:
+
+    print(player.location)
+
+    for line in textwrap.wrap(player.location.print_description()):
+        print(line)
+    print('\n')
+
+    command = input("What do you want to do? ('q' or 'quit' to quit)")
+
+    if command in ['n', 's', 'e', 'w']:
+        player.location = player.move_to(command, player.location)
+        continue
+
+    if command == 'q' or command == 'quit':
+        done = True
+
+    else:
+        print('I dont understand what you want, use n to move north, e to move east, s to move south, and w to move west \n')
