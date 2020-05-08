f2b90f7c5b6494abc3b98dbe6edb252b04e55cb3
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-05-07 21:13:42 -0500

    complete day2 MVP

--- src/room.py
+++ src/room.py
@@ -3,12 +3,19 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = items
 
     def __str__(self):
         return f"{self.name}"
 
     def print_description(self):
         return f"{self.description}"
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_item(self, item):
+        self.items.pop(item)
